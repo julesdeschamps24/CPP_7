12f5881b1d556aba31bc9e395c144b8b3b253641
judescha <julesdeschamps24@gmail.com>
2025-10-31 22:56:42 +0100

    fix ex00

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,22 +2,21 @@
 
 int main()
 {
-    int a = 1;
-    int b = 2;
+    int a = 2;
+    int b = 3;
 
-    std::cout << "a = " << a << " | b = " << b << "\nswap" << std::endl;
-    ::swap(a, b);
-    std::cout << "a = " << a << " | b = " << b << std::endl;
-    std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
-    std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+    ::swap( a, b );
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+    std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
 
-    std::string str_a = "aaaaaaaaaa";
-    std::string str_b = "bbbbbbbbbb";
+    std::string c = "chaine1";
+    std::string d = "chaine2";
 
-    std::cout << "\nstr_a = " << str_a << " | str_b = " << str_b << "\nswap" << std::endl;
-    ::swap(str_a, str_b);
-    std::cout << "str_a = " << str_a << " | str_b = " << str_b << std::endl;
-    std::cout << "min(str_a, str_b) = " << ::min(str_a, str_b) << std::endl;
-    std::cout << "max(str_a, str_b) = " << ::max(str_a, str_b) << std::endl;
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+    std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
 
+    return (0);
 }
